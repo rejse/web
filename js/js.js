851a069e0f38364df7ca864cc18907898d2f455b
rejse <47902260+rejse@users.noreply.github.com>
2024-03-05 00:35:58 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,30 @@
+<script>
+function changeurl(){
+var arr = new Array(
+
+"https://rejse.github.io/web/tp/000.gif",
+"https://rejse.github.io/web/tp/001.gif",
+"https://rejse.github.io/web/tp/002.gif",
+"https://rejse.github.io/web/tp/003.gif",
+"https://rejse.github.io/web/tp/004.gif",
+"https://rejse.github.io/web/tp/005.gif",
+"https://rejse.github.io/web/tp/006.gif",
+"https://rejse.github.io/web/tp/007.gif",
+"https://rejse.github.io/web/tp/008.gif",
+"https://rejse.github.io/web/tp/009.gif",
+"https://rejse.github.io/web/tp/010.gif",
+"https://rejse.github.io/web/tp/011.gif",
+"https://rejse.github.io/web/tp/012.gif",
+"https://rejse.github.io/web/tp/013.gif",
+"https://rejse.github.io/web/tp/014.gif",
+"https://rejse.github.io/web/tp/015.gif",
+"https://rejse.github.io/web/tp/016.gif",
+"https://rejse.github.io/web/tp/017.gif",
+"https://rejse.github.io/web/tp/018.gif",
+"https://rejse.github.io/web/tp/019.gif",
+
+);
+window.location.href = arr[Math.floor(Math.random() * arr.length)];
+}
+changeurl(); 
+</script>
